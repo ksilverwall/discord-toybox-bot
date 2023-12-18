2375c3e55a169b15d45bbf027ed0bac1ca7b3712
Kazuki Tsunemi <ksilverwall@gmail.com>
2023-12-18 20:35:47 +0900

    Add sheet load code

--- sample.py
+++ sample.py
@@ -1,18 +1,53 @@
-import discord
 import os
+
+import random
+import discord
+import gspread
 from dotenv import load_dotenv
+from oauth2client.service_account import ServiceAccountCredentials
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
+sheet_key = os.getenv('SHEET_KEY')
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+scope = ['https://www.googleapis.com/auth/spreadsheets', 
+         'https://www.googleapis.com/auth/drive']
+
+
+def load_prompts():
+    creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
+    client = gspread.authorize(creds)
+
+    spreadsheet = client.open_by_key(sheet_key)
+    worksheet = spreadsheet.sheet1
+
+    col_status = worksheet.col_values(1)
+    col_title = worksheet.col_values(3)
+    col_from = worksheet.col_values(4)
+
+    buffer = []
+    for s, t, f in zip(col_status, col_title, col_from):
+        if s != '使用済':
+            continue
+
+        buffer.append({
+            'prompt': t,
+            'post_by': f,
+        })
+    
+    return buffer
+
 
 async def process_toybox(message, words):
-    await message.channel.send(f'Hello!: {words}')
+    prompts = load_prompts()
+    p = random.choice(prompts)
+
+    await message.channel.send(f"お題: {p['prompt']}(by{p['post_by']})")
 
 
 @client.event
