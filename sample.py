1df6aa813331f8f82df540d4c61dc061112c7b25
Kazuki Tsunemi <ksilverwall@gmail.com>
2023-12-31 19:42:45 +0900

    Use cog

--- sample.py
+++ sample.py
@@ -24,7 +24,7 @@ sheet_key = secrets.get('SHEET_KEY')
 intents = discord.Intents.default()
 intents.message_content = True
 
-discord_client = commands.Bot(command_prefix='/toybox ', intents=intents)
+bot = commands.Bot(command_prefix='/toybox ', intents=intents)
 
 scope = [
     'https://www.googleapis.com/auth/spreadsheets',
@@ -57,56 +57,66 @@ def load_prompts():
     return buffer
 
 
-@discord_client.event
-async def on_ready():
-    print(f'We have logged in as {discord_client.user}')
-
-
-@discord_client.command()
-async def ogiri(ctx):
-    prompts = load_prompts()
-    p = random.choice(prompts)
-    await ctx.send(f"お題: {p['prompt']}(by{p['post_by']})")
-
-
 member_buffer = []
 
-@discord_client.command()
-async def insider(ctx):
-    view = View()
 
-    async def button_callback(interaction: discord.Interaction):
-        member_buffer.append(interaction.user)
+class ToyBoxCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def ogiri(self, ctx):
+        prompts = load_prompts()
+        p = random.choice(prompts)
+        await ctx.send(f"お題: {p['prompt']}(by{p['post_by']})")
+
+    @commands.command()
+    async def insider(self, ctx):
+        view = View()
+
+        async def button_callback(interaction: discord.Interaction):
+            member_buffer.append(interaction.user)
+            member_names = [m.global_name for m in member_buffer]
+            message = f"{interaction.user.global_name}が参加！ 現在：{member_names}"
+            await interaction.response.send_message(message)
+
+        async def on_submit(interaction: discord.Interaction):
+            if len(member_buffer) < 3:
+                await interaction.response.send_message("十分な数の参加者が居ません")
+                return
+
+            shuffled = member_buffer[:]
+            random.shuffle(shuffled)
+
+            master = shuffled[0]
+            insider_user = shuffled[1]
+
+            await master.send("インサイダー・ゲーム：あなたがマスターです")
+            await insider_user.send("インサイダー・ゲーム：あなたがインサイダーです")
+            for u in shuffled[2:]:
+                await u.send("インサイダー・ゲーム：あなたは市民です")
+
+            await interaction.response.send_message("ロールを配布しました")
+
+        entry_button = Button(label="参加", style=discord.ButtonStyle.primary)
+        entry_button.callback = button_callback
+        submit_button = Button(label="締切", style=discord.ButtonStyle.primary)
+        submit_button.callback = on_submit
+        view.add_item(entry_button)
+        view.add_item(submit_button)
+
         member_names = [m.global_name for m in member_buffer]
-        message = f"{interaction.user.global_name}が参加！ 現在：{member_names}"
-        await interaction.response.send_message(message)
-
-    async def on_submit(interaction: discord.Interaction):
-        if len(member_buffer) < 3:
-            await interaction.response.send_message("十分な数の参加者が居ません")
-
-        shuffled = member_buffer[:]
-        random.shuffle(shuffled)
-
-        master = shuffled[0]
-        insider_user = shuffled[1]
-
-        await master.send("インサイダー・ゲーム：あなたがマスターです")
-        await insider_user.send("インサイダー・ゲーム：あなたがインサイダーです")
-        for u in shuffled[2:]:
-            await u.send("インサイダー・ゲーム：あなたは市民です")
-
-        await interaction.response.send_message("ロールを配布しました")
-
-    entry_button = Button(label="参加", style=discord.ButtonStyle.primary)
-    entry_button.callback = button_callback
-    submit_button = Button(label="締切", style=discord.ButtonStyle.primary)
-    submit_button.callback = on_submit
-    view.add_item(entry_button)
-    view.add_item(submit_button)
-
-    member_names = [m.global_name for m in member_buffer]
-    await ctx.send(f'インサイダー・ゲームを始めます:({member_names})', view=view)
+        await ctx.send(f'インサイダー・ゲームを始めます:({member_names})', view=view)
 
 
-discord_client.run(token)
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+
+@bot.event
+async def setup_hook():
+    await bot.add_cog(ToyBoxCog(bot))
+
+
+bot.run(token)
